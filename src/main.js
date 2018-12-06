97906b5272c531a4fc21605eec0f071ede29e632
etechdevkant <devkant@excellencetechnologies.in>
2018-12-06 12:20:26 +0530

    home completed

--- src/main.js
+++ src/main.js
@@ -1,5 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue' //bootstrapVUE
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
+
+Vue.use(BootstrapVue); //bootstrapVUE
 
 Vue.config.productionTip = false
 
